67aa1984a1b1564a8d0288b1e6d37e7ef6a97c5c
patrikk001 <p.korba09@gmail.com>
2026-09-18 16:45:29 +0200

    Add triangle pattern printing in trojuholnik.c
    Prints a right-angled triangle pattern using asterisks.

---
+++ trojuholnik.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+
+int main() {
+    for (int i = 0; i < 1; i++) {
+        printf("*");
+    }
+    printf("\n");
+
+    for (int j = 0; j < 2; j++) {
+        printf("*");
+    }
+    printf("\n");
+
+    for (int g = 0; g < 3; g++) {
+        printf("*");
+    }
+    printf("\n");
+
+    for (int k = 0; k < 4; k++) {
+        printf("*");
+    }
+    printf("\n");
+
+    for (int s = 0; s < 5; s++) {
+        printf("*");
+    }
+
+    return 0;
+}
